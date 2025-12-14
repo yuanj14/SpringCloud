2c0f3b2fbea5b3a6e3a36d027874b9ce8747470f
wowo <1959558509@qq.com>
2025-12-14 18:11:13 +0800

    feat:redis+serialize

--- sky-server/src/main/java/com/sky/config/OssConfiguration.java
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -4,7 +4,6 @@ package com.sky.config;
 import com.sky.properties.AliOssProperties;
 import com.sky.utils.AliOssUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,44 @@
+
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("admin/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     *
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺的营业状态")
+    public Result setStatus(@PathVariable("status") Integer status) {
+        redisTemplate.opsForValue().set("status", status);
+        return Result.success();
+    }
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get("status");
+        return Result.success(status);
+    }
+}
